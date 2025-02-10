963725ca6b8e4f273f222cce3974c62f663cd506
Olesya <fenixa@inbox.ru>
2025-02-10 17:13:12 +0300

    add oppotunity to change maininf by admin on landing page

--- src/apps/landing/urls/urls.py
+++ src/apps/landing/urls/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from landing.views import views
+from apps.landing.views import views
 
 app_name = "landing"
 
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
+    path("aboutme/create", views.AboutMeCreate.as_view(), name="about-me-create"),
+    # URL для сохранения текста
+    path('save_main_inf_text/', views.SaveMainInfTextView.as_view(), name='save_main_inf_text'),
+
+    # URL для сохранения изображения
+    path('save_main_inf_img/', views.SaveMainInfImgView.as_view(), name='save_main_inf_img'),
+    #path('save_main_inf/', views.save_main_inf, name='save-main-inf'),
 ]
--- src/apps/landing/views/views.py
+++ src/apps/landing/views/views.py
@@ -1,7 +1,70 @@
-from django.views.generic import TemplateView
+import json
 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import ValidationError
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.generic import TemplateView, CreateView, DeleteView, UpdateView, FormView
+
+from landing.forms import MainInfForm, AboutMeForm, ContentForm, ProductForm
 from landing.models import MainInf, AboutMe, Product, Content
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+from apps.core.permissions import IsSuperuserStaffAdmin
+
+
+class SaveMainInfTextView(View):
+    @method_decorator(csrf_exempt)
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        try:
+            # Изменение полей модели
+            main_inf = MainInf.objects.first()
+            main_inf.title = data['title']
+            main_inf.text = data['text']
+            main_inf.full_clean()
+            main_inf.save()
+
+            return JsonResponse({'success': True})
+
+        except ValidationError as e:
+            return JsonResponse({'success': False, 'errors': e.message_dict})
+
+        except MainInf.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'MainInf not found'})
+
+        except Exception as e:
+            # Перехватываем все другие ошибки и выводим
+            return JsonResponse({'success': False, 'error': str(e)})
+
+class SaveMainInfImgView(View):
+    @method_decorator(csrf_exempt)
+    def post(self, request, *args, **kwargs):
+        if request.FILES:
+            image = request.FILES.get('image')
+            if image:
+                # Сохранение изображения
+                main_inf = MainInf.objects.first()
+                main_inf.image = image
+
+                try:
+                    main_inf.full_clean()  # Валидируем модель, включая изображение
+                    main_inf.save()
+                    return JsonResponse({'success': True})
+                except ValidationError as e:
+                    return JsonResponse({'success': False, 'errors': e.message_dict})
+            else:
+                return JsonResponse({'success': False, 'errors': 'No image uploaded'})
+        return JsonResponse({'success': False, 'errors': 'Invalid request'})
+
+class AdminRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
+    """
+    Mixin для проверки прав администратора.
+    """
+    def test_func(self):
+        return IsSuperuserStaffAdmin().has_permission(self.request, self)
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -18,7 +81,7 @@ class HomePageView(TemplateView):
         # Получаем все записи из Product
         product_list = Product.objects.all()
 
-        # Получаем все записи из ModelC
+        # Получаем все записи из Content
         content_list = Content.objects.all()
 
         context['main_inf'] = main_inf
@@ -27,3 +90,80 @@ class HomePageView(TemplateView):
         context['content_list'] = content_list
 
         return context
+
+# MainInf: Редактирование полей
+class MainInfEditView(AdminRequiredMixin, FormView):
+    template_name = 'landing/maininf_edit.html'
+    form_class = MainInfForm
+    success_url = reverse_lazy('landing:home')
+
+    def get_form(self, *args, **kwargs):
+        main_inf = MainInf.objects.first()
+        return self.form_class(instance=main_inf, **self.get_form_kwargs())
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+# AboutMe: Создание, редактирование, удаление
+class AboutMeCreate(AdminRequiredMixin, CreateView):
+    model = AboutMe
+    form_class = AboutMeForm
+    template_name = 'landing/about_me_form.html'
+    success_url = reverse_lazy('landing:home')
+
+
+class AboutMeUpdate(AdminRequiredMixin, UpdateView):
+    model = AboutMe
+    form_class = AboutMeForm
+    template_name = 'landing/about_me_form.html'
+    success_url = reverse_lazy('landing:home')
+
+
+class AboutMeDelete(AdminRequiredMixin, DeleteView):
+    model = AboutMe
+    template_name = 'landing/confirm_delete.html'
+    success_url = reverse_lazy('landing:home')
+
+
+# Content: Создание, редактирование, удаление
+class ContentCreate(AdminRequiredMixin, CreateView):
+    model = Content
+    form_class = ContentForm
+    template_name = 'landing/content_form.html'
+    success_url = reverse_lazy('landing:home')
+
+
+class ContentUpdate(AdminRequiredMixin, UpdateView):
+    model = Content
+    form_class = ContentForm
+    template_name = 'landing/content_form.html'
+    success_url = reverse_lazy('landing:home')
+
+
+class ContentDelete(AdminRequiredMixin, DeleteView):
+    model = Content
+    template_name = 'landing/confirm_delete.html'
+    success_url = reverse_lazy('landing:home')
+
+
+# Product: Создание, редактирование, удаление
+class ProductCreate(AdminRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'landing/product_form.html'
+    success_url = reverse_lazy('landing:home')
+
+
+class ProductUpdate(AdminRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'landing/product_form.html'
+    success_url = reverse_lazy('landing:home')
+
+
+class ProductDelete(AdminRequiredMixin, DeleteView):
+    model = Product
+    template_name = 'landing/confirm_delete.html'
+    success_url = reverse_lazy('landing:home')
